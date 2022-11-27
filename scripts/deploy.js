7dfb3400e0294deb490ca3dd39de8a0b80630c69
Skkay <skkay.thomas@gmail.com>
2022-11-27 19:36:13 +0100

    feat(contract): Add deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,22 @@
+import hre from 'hardhat@esm';
+
+const main = async () => {
+    const name = 'SupRailRoad - Bus Ticket';
+    const symbol = 'SRRBT';
+    const ipfsMetadataUri = 'ipfs://QmeKvYa8F2srzp2WNVPwNCeDYvXs6bxR4hSiqR4T4pzuRt';
+
+    const srrContractFactory = await hre.ethers.getContractFactory('GreedyGeese');
+    const srrContract = await srrContractFactory.deploy(name, symbol, ipfsMetadataUri);
+
+    await srrContract.deployed();
+
+    console.log(`${name} (${symbol}) contract deployed to: ${srrContract.address}`);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    })
+;
