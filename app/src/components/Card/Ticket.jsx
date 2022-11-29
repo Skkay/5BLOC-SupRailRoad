1f006526997c7331c27484c304d8e42404c3fef8
Skkay <skkay.thomas@gmail.com>
2022-11-29 20:49:18 +0100

    feat(Ticket): Add buying NFT ticket logic

--- app/src/components/Card/Ticket.jsx
+++ app/src/components/Card/Ticket.jsx
@@ -1,7 +1,46 @@
-import busTicketImg from '../../assets/bus-ticket_placeholder.png';
-import BuyTicket from '../Button/BuyTicket';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { BigNumber, ethers } from 'ethers';
+import { formatUnits } from 'ethers/lib/utils';
 
-const Ticket = ({ title, price, currency = 'GoerliETH' }) => {
+import busTicketImg from '../../assets/bus-ticket_placeholder.png';
+import ticketContractMetadata from '../../artifacts/contracts/Ticket.sol/Ticket.json';
+
+const Ticket = ({ title, contractAddress, currency = 'ETH' }) => {
+    const clientAddress = useSelector((state) => state.address.value);
+
+    const [cost, setCost] = useState(null);
+
+    const fetchCost = () => {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const contract = new ethers.Contract(contractAddress, ticketContractMetadata.abi, provider);
+
+        contract.cost()
+            .then((cost) => setCost(String(cost)))
+            .catch((err) => console.err(err))
+        ;
+    }
+
+    const handleBuyButtonClick = () => {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+
+        const contract = new ethers.Contract(contractAddress, ticketContractMetadata.abi, signer);
+        const overrides = {
+            from:  clientAddress,
+            value: cost,
+        }
+
+        contract.mint(clientAddress, overrides)
+            .then((res) => console.log(res))
+            .catch((err) => console.error(err))
+        ;
+    };
+
+    useEffect(() => {
+        fetchCost();
+    }, []);
+
     return (
         <div className="w-80 bg-white border rounded-lg shadow-xl">
             <div className="border border-gray-200 w-fit mx-auto my-4 p-4">
@@ -9,9 +48,9 @@ const Ticket = ({ title, price, currency = 'GoerliETH' }) => {
             </div>
             <div className="border-t mt-5 p-4">
                 <h1 className="text-xl font-semibold">{title}</h1>
-                <div className="text-4xl mt-3">{price} {currency}</div>
+                <div className="text-4xl mt-3">{cost ? formatUnits(BigNumber.from(cost), 'ether') : '-'} {currency}</div>
                 <div className="mt-12 w-fit mx-auto">
-                    <BuyTicket className={'bg-purple-800 disabled:bg-purple-400 text-white rounded-md px-4 py-3'} price={price} />
+                    <button className='bg-purple-800 disabled:bg-purple-400 text-white rounded-md px-4 py-3' onClick={handleBuyButtonClick} disabled={clientAddress === ''}>Buy Ticket</button>
                 </div>
             </div>
         </div>
