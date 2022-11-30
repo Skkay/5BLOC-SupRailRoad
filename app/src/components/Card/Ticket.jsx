90e2b308a11d64af6a796fc0347b949b4f52c1da
Skkay <skkay.thomas@gmail.com>
2022-11-30 20:24:12 +0100

    feat(Ticket): Load metadata from Alchemy

--- app/src/components/Card/Ticket.jsx
+++ app/src/components/Card/Ticket.jsx
@@ -2,14 +2,17 @@ import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { BigNumber, ethers } from 'ethers';
 import { formatUnits } from 'ethers/lib/utils';
+import { Network, Alchemy } from "alchemy-sdk";
 
-import busTicketImg from '../../assets/bus-ticket_placeholder.png';
 import ticketContractMetadata from '../../artifacts/contracts/Ticket.sol/Ticket.json';
 
-const Ticket = ({ title, contractAddress, currency = 'ETH' }) => {
+const ALCHEMY_API_KEY = import.meta.env.VITE_ALCHEMY_API_KEY;
+
+const Ticket = ({ contractAddress, currency = 'ETH' }) => {
     const clientAddress = useSelector((state) => state.address.value);
 
     const [cost, setCost] = useState(null);
+    const [metadata, setMetadata] = useState(null);
 
     const fetchCost = () => {
         const provider = new ethers.providers.Web3Provider(ethereum);
@@ -17,6 +20,18 @@ const Ticket = ({ title, contractAddress, currency = 'ETH' }) => {
 
         contract.cost()
             .then((cost) => setCost(String(cost)))
+            .catch((err) => console.err(err))
+        ;
+    }
+
+    const fetchMetadata = () => {
+        const alchemy = new Alchemy({
+            apiKey: ALCHEMY_API_KEY,
+            network: Network.ETH_GOERLI,
+        });
+
+        alchemy.nft.getNftMetadata(contractAddress, 1)
+            .then((res) => setMetadata(res))
             .catch((err) => console.err(err))
         ;
     }
@@ -39,15 +54,17 @@ const Ticket = ({ title, contractAddress, currency = 'ETH' }) => {
 
     useEffect(() => {
         fetchCost();
+        fetchMetadata();
     }, []);
 
     return (
         <div className="w-80 bg-white border rounded-lg shadow-xl">
             <div className="border border-gray-200 w-fit mx-auto my-4 p-4">
-                <img src={busTicketImg} alt={`${title} image`} width="220" height="220" />
+                <img src={metadata ? `https://ipfs.io/ipfs/${metadata.rawMetadata.image.substring(7)}` : ''} alt={`${metadata ? metadata.title : ''} image`} width="220" height="220" />
             </div>
             <div className="border-t mt-5 p-4">
-                <h1 className="text-xl font-semibold">{title}</h1>
+                <h1 className="text-xl font-semibold">{metadata ? metadata.title : '-'} ({metadata ? metadata.contract.symbol : '-'})</h1>
+                <div>{metadata ? metadata.description : '-'}</div>
                 <div className="text-4xl mt-3">{cost ? formatUnits(BigNumber.from(cost), 'ether') : '-'} {currency}</div>
                 <div className="mt-12 w-fit mx-auto">
                     <button className='bg-purple-800 disabled:bg-purple-400 text-white rounded-md px-4 py-3' onClick={handleBuyButtonClick} disabled={clientAddress === ''}>Buy Ticket</button>
--- app/src/components/TicketContainer/TicketContainer.jsx
+++ app/src/components/TicketContainer/TicketContainer.jsx
@@ -7,9 +7,9 @@ const METRO_TICKET_CONTRACT_ADDRESS = import.meta.env.VITE_METRO_TICKET_CONTRACT
 const TicketContainer = () => {
     return (
         <div className="flex flex-wrap justify-center gap-4">
-            <Ticket title={'Bus Ticket'} contractAddress={BUS_TICKET_CONTRACT_ADDRESS} currency={'GoerliETH'} />
-            <Ticket title={'Train Ticket'} contractAddress={TRAIN_TICKET_CONTRACT_ADDRESS} currency={'GoerliETH'} />
-            <Ticket title={'Metro Ticket'} contractAddress={METRO_TICKET_CONTRACT_ADDRESS} currency={'GoerliETH'} />
+            <Ticket contractAddress={BUS_TICKET_CONTRACT_ADDRESS} currency={'GoerliETH'} />
+            <Ticket contractAddress={TRAIN_TICKET_CONTRACT_ADDRESS} currency={'GoerliETH'} />
+            <Ticket contractAddress={METRO_TICKET_CONTRACT_ADDRESS} currency={'GoerliETH'} />
         </div>
     );
 };
