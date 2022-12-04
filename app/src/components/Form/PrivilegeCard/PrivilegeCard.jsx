cf3a0db90df2dbec19477ab58fc931593ae17f7f
Skkay <skkay.thomas@gmail.com>
2022-12-04 20:35:46 +0100

    feat(PrivilegeCard): Add 'symbol' input

--- app/src/components/Form/PrivilegeCard/PrivilegeCard.jsx
+++ app/src/components/Form/PrivilegeCard/PrivilegeCard.jsx
@@ -18,6 +18,13 @@ const PrivilegeCard = () => {
                     "/>
                 </label>
                 <label>
+                    <span className="block text-sm font-medium text-slate-700">Symbol</span>
+                    <input {...register('symbol', { required: true })} type="text" className="block mt-1 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm
+                        focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                        disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                    "/>
+                </label>
+                <label>
                     <span className="block text-sm font-medium text-slate-700">Description</span>
                     <input {...register('description', { required: true })} type="text" className="block mt-1 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm
                         focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
