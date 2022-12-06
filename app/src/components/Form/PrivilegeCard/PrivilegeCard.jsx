d7be51416a644d4222aebddc83dcadb5bad66d7c
Skkay <skkay.thomas@gmail.com>
2022-12-06 19:51:30 +0100

    feat(PrivilegeCard): Handle contract deployment

--- app/src/components/Form/PrivilegeCard/PrivilegeCard.jsx
+++ app/src/components/Form/PrivilegeCard/PrivilegeCard.jsx
@@ -1,10 +1,24 @@
 import { useForm } from "react-hook-form";
+import { ethers } from 'ethers';
+import { parseUnits } from 'ethers/lib/utils';
+
+import privilegeCardContractMetadata from '../../../artifacts/contracts/PrivilegeCard.sol/PrivilegeCard.json';
+
+const ACCOUNT_PRIVATE_KEY = import.meta.env.VITE_ACCOUNT_PRIVATE_KEY;
 
 const PrivilegeCard = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
 
-    const onSubmit = (data) => {
-        console.log(data);
+    const onSubmit = async (data) => {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const wallet = new ethers.Wallet(ACCOUNT_PRIVATE_KEY, provider);
+
+        const contractFactory = new ethers.ContractFactory(privilegeCardContractMetadata.abi, privilegeCardContractMetadata.bytecode, wallet);
+        const contract = await contractFactory.deploy(data.name, data.symbol, data.metadataUri, parseUnits(data.price, 'ether'), data.quantity, data.discount);
+
+        await contract.deployed();
+
+        console.log(`Contract deployed to: ${contract.address}`); // TODO
     }
 
     return (
@@ -33,21 +47,21 @@ const PrivilegeCard = () => {
                 </label>
                 <label>
                     <span className="block text-sm font-medium text-slate-700">Price</span>
-                    <input {...register('price', { required: true, min: 0 })} type="number" className="block mt-1 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm
+                    <input {...register('price', { required: true, min: 0 })} type="number" min={0} step="any" className="block mt-1 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm
                         focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
                         disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
                     "/>
                 </label>
                 <label>
                     <span className="block text-sm font-medium text-slate-700">Quantity</span>
-                    <input {...register('quantity', { required: true, min: 0 })} type="number" className="block mt-1 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm
+                    <input {...register('quantity', { required: true, min: 0 })} type="number" min={0} className="block mt-1 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm
                         focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
                         disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
                     "/>
                 </label>
                 <label>
                     <span className="block text-sm font-medium text-slate-700">% Discount</span>
-                    <input {...register('discount', { required: true, min: 0, max: 100 })} type="number" className="block mt-1 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm
+                    <input {...register('discount', { required: true, min: 0, max: 100 })} type="number" min={0} max={100} className="block mt-1 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm
                         focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
                         disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
                     "/>
